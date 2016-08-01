8c6606d809f5c64f26e69055216b7b56135b363b
Hugo López Fernández <hlfernandez@uvigo.es>
2016-08-01 10:33:57 +0100

    Use image filename (without extension) as title

--- php/ImagesHelper.php
+++ php/ImagesHelper.php
@@ -12,11 +12,12 @@
 			for($x = 0; $x <= $total; $x++): 
 			
 				if ($files[$x] != '.' && $files[$x] != '..' && is_file($this->imagesPath."/".$files[$x]) && exif_imagetype($this->imagesPath."/".$files[$x])==3) {
+					$filename = pathinfo($this->imagesPath."/$files[$x]", PATHINFO_FILENAME);
 					echo '<div class="col-lg-3 col-md-4 col-xs-6 thumb">';
 					echo '<a class="fancybox thumbnail" rel="gallery1" href="';
 					echo $this->imagesPath."/$files[$x]";
 					echo '" title="';
-					echo "$files[$x]";
+					echo "$filename";
 					echo '">';
 					echo '<img class="img-responsive lazy" data-src="';
 					echo $this->imagesPath."/$files[$x]";
